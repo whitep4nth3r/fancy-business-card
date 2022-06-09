2bfb13c3406bf94cf584ca62eda2b8d290438466
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-06-09 09:45:12 +0100

    Update CLI message

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 // 👆 Used to tell Node.js that this is a CLI tool
 
-console.log("My fancy business card!");
+console.log(
+  "Create a CLI tool to output a business card in the terminal!\n\nView the tutorial ➞\n\nhttps://whitep4nth3r.com/blog/build-a-business-card-cli-tool/",
+);
